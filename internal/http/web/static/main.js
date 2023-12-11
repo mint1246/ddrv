de6192c450aa1c61db26d31080187233b674e974
mint <60389596+mint1246@users.noreply.github.com>
2023-12-11 22:28:46 +0200

    Update main.js

--- internal/http/web/static/main.js
+++ internal/http/web/static/main.js
@@ -96,7 +96,35 @@ app.controller('controller', ['$scope', 'FMService', '$interval', function ($sco
             $scope.btnDelete = false;
         }
     }
-
+// Add a variable to store the toggle state
+$scope.embed = false;
+// Add a function to toggle the embed state
+$scope.toggleEmbed = function () {
+  $scope.embed = !$scope.embed;
+};
+// Modify the open function to check the embed state
+$scope.open = function (file) {
+  const url = `${$scope.baseURL}/files/${file.id}/${file.name}`;
+  // Create a regular expression to match photo formats
+  const photoRegex = /\.(jpg|jpeg|png|gif|bmp|svg)$/i;
+  // Test if the url matches the photoRegex
+  if (photoRegex.test(url)) {
+    // If it is a photo, check the embed state
+    if ($scope.embed) {
+      // If embed is true, embed it in the page
+      const img = document.createElement("img");
+      img.src = url;
+      img.alt = file.name;
+      document.body.appendChild(img);
+    } else {
+      // If embed is false, open it in a new tab
+      window.open(url, "_blank");
+    }
+  } else {
+    // If it is not a photo, open it in a new tab
+    window.open(url, "_blank");
+  }
+};
     $scope.load = function (id) {
         FMService.getDir(id).then((directory) => {
             $scope.$apply(() => $scope.directory = directory)
