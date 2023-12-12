2afece57b8374563ad83f0e23c523362f1b7fdc9
mint <60389596+mint1246@users.noreply.github.com>
2023-12-12 11:58:20 +0200

    a

--- internal/http/web/static/photos.js
+++
@@ -1,9 +0,0 @@
-// Get the files array from the directory object
-const files = $scope.directory.files;
-// Filter the files that have the image MIME type
-const photoFiles = files.filter(file => {
-  // Check if the file type starts with image/
-  return file.type.startsWith("image/");
-});
-// Log the result to the console
-console.log(photoFiles);
